dfe2c6dda71e30a18dc99ba2c8c4f8dbbe96ce17
Raunak Dubey <your-email@example.com>
2026-08-03 15:07:29 +0530

    Some changes for Deployment Purpose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"
 
 import authRoute from "./routes/auth.route.js";
 import userRoute from "./routes/user.route.js";
@@ -14,6 +15,8 @@ dotenv.config();
 const PORT=process.env.PORT;
 const app=express();
 
+const __dirname=path.resolve();
+
 app.use(cors({
     origin:"http://localhost:5173",
     credentials:true //allow frontend to send the cookies
@@ -24,6 +27,14 @@ app.use(cookieParser());
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
 app.use("/api/chat",chatRoute)
+
+if(process.env.NODE_ENV==="production"){
+app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get((req,res)=>{
+    res.sendFile(path.join(__dirname,"../frontend/dist/index.html"))
+})
+}
 
 
 app.listen(PORT,()=>{
